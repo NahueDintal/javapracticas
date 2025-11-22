61fab4cbea8ff362c283ee33dadd2c63cafef8af
nahuedintal <nahue.dintal@gmail.com>
2025-11-22 17:55:36 -0300

    primer commit

---
+++ proyecto-simple/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Ingrese el número a sumar");
+    Scanner scanner = new Scanner(System.in);
+    int valor = scanner.nextInt();
+
+  }
+}
