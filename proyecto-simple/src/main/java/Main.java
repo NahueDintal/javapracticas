1f657e9efd2e3bcfa17711c1378ad8c460fa7def
nahuedintal <nahue.dintal@gmail.com>
2025-11-25 19:20:32 -0300

    practicando clases

--- proyecto-simple/src/main/java/Main.java
+++ proyecto-simple/src/main/java/Main.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Ingrese el número a sumar");
-    Scanner scanner = new Scanner(System.in);
-    int valor = scanner.nextInt();
 
+    Libro libro1 = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967);
+    Libro libro2 = new Libro("1984", "George Orwell", 1949);
+    Libro libro3 = new Libro("El Quijote", "Miguel de Cervantes", 1605);
+
+    libro1.mostrarInfo();
+    libro2.mostrarInfo();
+    libro3.mostrarInfo();
   }
 }
