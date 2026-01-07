f8d52f45172920cede1f174a04f8d1e65f9feb5f
nahuedintal <nahue.dintal@gmail.com>
2026-01-07 12:57:20 -0300

    practica de algoritmos de busqueda y ordenamiento

--- algoritmos/src/main/java/Busqueda.java
+++ algoritmos/src/main/java/Busqueda.java
@@ -26,4 +26,5 @@ public class Busqueda {
     }
     return -1;
   }
+
 }
--- algoritmos/src/main/java/Main.java
+++ algoritmos/src/main/java/Main.java
@@ -1,7 +1,19 @@
 public class Main {
   public static void main(String[] args) {
-    int[] numeros = { 10, 15, 20, 25, 30, 35 };
-    int buscar = 21;
+    int[] numeros = { 10, 19, 18, 9, 15, 7, 25, 30, 35 };
+    int buscar = 18;
+
+    // Ordenamiento.bubbleSort(numeros);
+    // System.out.println("Array ordenado por bubble sort");
+
+    // Ordenamiento.selectionSort(numeros);
+    // System.out.println("Array ordenado por selection sort");
+
+    // Ordenamiento.insertionSort(numeros);
+    // System.out.println("Array ordenando por insercisión");
+
+    // Ordenamiento.insertionSort(numeros);
+    // System.out.println("Array ordenado por insersión");
 
     int resultadoBinario = Busqueda.binaria(numeros, buscar);
 
@@ -17,4 +29,5 @@ public class Main {
             ? "Elemento no encontrado"
             : "Elemento contrado en la posicion: " + resultadoLineal);
   }
+
 }
