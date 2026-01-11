b9de644d643424a4cb9d582fbe7fd1215088aea9
nahuedintal <nahue.dintal@gmail.com>
2026-01-11 12:29:51 -0300

    agregado de prueba para meme

--- pruebas/src/main/java/Main.java
+++ pruebas/src/main/java/Main.java
@@ -1,29 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    int a = 10;
-    int b = 5;
-    int c = 7;
-    int tmp = 0;
-
-    if (a < b) {
-      tmp = a;
-      a = b;
-      b = tmp;
+    var michi = 4;
+    for (int i = 0; i < michi; i++) {
+      System.out.println("Michi de Shrodringer!!");
     }
-    if (b < c) {
-      tmp = b;
-      b = c;
-      c = tmp;
-    }
-    if (a < b) {
-      tmp = a;
-      a = b;
-      b = tmp;
-    }
-    System.out.println(tmp);
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(c);
-
   }
 }
