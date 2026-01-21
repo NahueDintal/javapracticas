d0a643a356d6e580dcbb1ec0671aa0dedde7b6e5
nahuedintal <nahue.dintal@gmail.com>
2026-01-21 08:41:40 -0300

    agregado de ejercicio con try catch, y agregado de servicios en cerrajeria

---
+++ cerrajeria/src/main/java/Servicio.java
@@ -0,0 +1,13 @@
+// servicio
+// busqueda de productos por distintos medios, por id, por marca, etc
+// por id
+// por marca
+// por precio
+// aciones abm sobre productos
+// agregar
+// bajar
+// modificar
+// calculos de precios, etc
+// persistencia, guardado y actualizaciones
+// historial de cambios
+// generacion de reportes
--- pruebas/src/main/java/Main.java
+++ pruebas/src/main/java/Main.java
@@ -1,8 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    var michi = 4;
-    for (int i = 0; i < michi; i++) {
-      System.out.println("Michi de Shrodringer!!");
+    Scanner scanner = new Scanner(System.in);
+    try {
+      System.out.println("Ingrese dividendo");
+      int dividendo = scanner.nextInt();
+
+      System.out.println("Ingrese divisor");
+      int divisor = scanner.nextInt();
+
+      int resultado = dividendo / divisor;
+      System.out.println("Resultado: " + resultado);
+
+    } catch (ArithmeticException e) {
+      System.out.println("Error: No se puede dividir por cero!");
+
+    } catch (InputMismatchException e) {
+      System.out.println("Error: Debe ingresar números enteros!");
+
+    } finally {
+      System.out.println("Operacion finalizada!");
+      scanner.close();
+
     }
   }
 }
