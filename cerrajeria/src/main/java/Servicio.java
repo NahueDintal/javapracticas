60bc97bcf012e74c3ab80fb885b7dfcbb496e475
nahuedintal <nahue.dintal@gmail.com>
2026-02-02 17:30:39 -0300

    intento de service

--- cerrajeria/src/main/java/Servicio.java
+++ cerrajeria/src/main/java/Servicio.java
@@ -1,8 +1,28 @@
 // servicio
+
 // busqueda de productos por distintos medios, por id, por marca, etc
+
 // por id
+public int BusquedaProducto(int id) {
+  if (id == getId()) {
+    mostrarInformacion();
+  } else {
+    throw new IllegalArgument("No se encuentra el producto.");
+  }
+
+}
+
 // por marca
+public String BusquedaMarca(String marca) {
+  if (marca == getMarca()) {
+    mostrarInformacion();
+  } else {
+    throw new IllegalArgument("No se encuentra la Marca ingresada.");
+  }
+}
+
 // por precio
+
 // aciones abm sobre productos
 // agregar
 // bajar
