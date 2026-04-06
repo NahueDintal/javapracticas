2232ca1fd80fae2e4999ceb7d4a2f9d2f98d18e0
NaCk3rZ! <nahue.dintal@gmail.com>
2026-04-06 14:34:50 -0300

    agreagado de jc new en algoritmos

--- algoritmos/src/main/java/Main.java
+++ algoritmos/src/main/java/Main.java
@@ -1,32 +1,5 @@
 public class Main {
-  public static void main(String[] args) {
-    int[] numeros = { 10, 19, 18, 9, 15, 7, 25, 30, 35 };
-    int buscar = 18;
-
-    // Ordenamiento.bubbleSort(numeros);
-    // System.out.println("Array ordenado por bubble sort");
-
-    // Ordenamiento.selectionSort(numeros);
-    // System.out.println("Array ordenado por selection sort");
-
-    // Ordenamiento.insertionSort(numeros);
-    // System.out.println("Array ordenando por insercisión");
-
-    // Ordenamiento.insertionSort(numeros);
-    // System.out.println("Array ordenado por insersión");
-
-    int resultadoBinario = Busqueda.binaria(numeros, buscar);
-
-    System.out.println(
-        resultadoBinario == -1
-            ? "Elemento no encontrado"
-            : "Elemento econtrado en posicion: " + resultadoBinario);
-
-    int resultadoLineal = Busqueda.lineal(numeros, buscar);
-
-    System.out.println(
-        resultadoLineal == -1
-            ? "Elemento no encontrado"
-            : "Elemento contrado en la posicion: " + resultadoLineal);
-  }
+    public static void main(String[] args) {
+        System.out.println("¡Hola Wachin! ¡Desde el directorio 'algoritmos' con Java 25!");
+    }
 }
