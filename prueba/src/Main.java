1ac8e8ec9946d98f71698edfbe8eedcd080b532a
NaCk3rZ! <nahue.dintal@gmail.com>
2026-07-04 20:03:40 -0300

    agregado de pruebas con jc para que jale java fx

--- prueba/src/Main.java
+++
@@ -1,4 +0,0 @@
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
---
+++ prueba/src/TestFX.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  @Override
+  public void start(Stage primaryStage) {
+    // tu código de la interfaz
+  }
+
+  public static void main(String[] args) {
+    launch(args); // ahora launch está definido porque Main extiende Application
+  }
+}
