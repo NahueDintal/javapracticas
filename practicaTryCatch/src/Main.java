c1e2c5cd41869929958312e2f69954cf3c279805
NaCk3rZ! <nahue.dintal@gmail.com>
2026-07-09 23:01:19 -0300

    re haciendo hotel para comenzar con estudio de poo

---
+++ practicaTryCatch/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("");
+
+    String datoIngresado = "";
+    // validar un dato, string
+
+    try {
+      System.out.println("El dato ingresado es: " + datoIngresado);
+    } catch (Exception e) {
+      // TODO: handle exception
+    }
+  }
+}
